ea439925f0c1793575c85ce2796b23f0c722f525
STRUDYGRASS <yifei-zhou@qq.com>
2019-06-19 22:59:56 +0800

    MAIN页面

--- src/MAIN_VIEW.java
+++ src/MAIN_VIEW.java
@@ -3,9 +3,15 @@ import cn.goktech.ShootGame;
 import javax.swing.*;
 
 public class MAIN_VIEW {
-    public int State = 0;
+    private JButton btn_ToInfo = null,btn_ToGame = null;
     public static void main(String[] args){
+        JFrame jFrame = new JFrame("java大项目");
+        MAIN_VIEW main_view = new MAIN_VIEW();
         ShootGame game = new ShootGame();
 
+    }
+    public MAIN_VIEW(){
+        btn_ToInfo = new JButton("我要看学籍");
+        btn_ToGame = new JButton("我要玩游戏");
     }
 }
